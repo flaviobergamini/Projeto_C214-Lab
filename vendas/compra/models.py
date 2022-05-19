3d8902f646747e168dc42dc5d7efe88c19a0b13b
= <=>
2022-05-18 23:58:55 -0300

    Mudando quantidade de caracteres dos atributos das tabelas compras e produto, e modificando forma de id de uuid4 para uuid1

--- vendas/compra/models.py
+++ vendas/compra/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-from uuid import uuid4
+from uuid import uuid1
 
 from cliente.models import Cliente
 from produto.models import Produto
 
 class Compra(models.Model):
-    idCompra = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    idCompra = models.UUIDField(primary_key=True, default=uuid1, editable=False)
     preco = models.FloatField()
-    tipoPagamento = models.CharField(max_length=10)
+    tipoPagamento = models.CharField(max_length=15)
     codigoRastreamento = models.CharField(max_length=15)
     quantidadeCoprada = models.IntegerField()
     cliente_cpf = models.ForeignKey(Cliente, on_delete=models.CASCADE, blank=True, null=True)
--- vendas/produto/models.py
+++ vendas/produto/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from uuid import uuid4
+from uuid import uuid1
 
 
 class Produto(models.Model):
-    idProduto = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    idProduto = models.UUIDField(primary_key=True, default=uuid1, editable=False)
     nome = models.CharField(max_length=45)
-    marca = models.CharField(max_length=10)
+    marca = models.CharField(max_length=20)
     preco = models.FloatField()
     quantidade = models.IntegerField(blank=True, null=True)
     
