7fed7f4c2a6df779d05712d93042b8e62d94cae9
G-Ilian <gbiel.boza@gmail.com>
2022-06-12 15:01:50 -0300

    Modificado models para atender as exigÃências do projeto, e adicionado testes unitários e de integração em tests_model_compra

--- vendas/compra/models.py
+++ vendas/compra/models.py
@@ -12,3 +12,28 @@ class Compra(models.Model):
     quantidadeCoprada = models.IntegerField()
     cliente_cpf = models.ForeignKey(Cliente, on_delete=models.CASCADE, blank=True, null=True)
     produto_id = models.ForeignKey(Produto, on_delete=models.CASCADE, blank=True, null=True)
+
+    # Metódos da classe
+    def __calcula_total_desconto(self):
+        """
+        As seguintes condições devem ser respeitada as seguintes regras
+        para a precificação do produto:
+
+        - Caso seja comprado a unidade, o preço deve ser mantido como anteriormente precificado
+
+        - Caso seja uma quantidade que esteja, 10<=qtd <=99 é concedido ao cliente um total de 5% de desconto
+
+        - Caso seja comprado a centena é concedido ao cliente um total de 15% de desconto
+      """
+
+        if (self.quantidadeCoprada >= 10 and self.quantidadeCoprada <= 99):
+            self.preco=self.preco - (self.preco * 0.05)
+            return self.quantidadeCoprada*self.preco;
+
+        if (self.quantidadeCoprada >= 100):
+            self.preco = self.preco - (self.preco * 0.15)
+            return self.quantidadeCoprada * self.preco;
+
+    def calcula_total(self):
+
+        return self.__calcula_total_desconto();
--- vendas/compra/tests_compra/tests_model_compra.py
+++ vendas/compra/tests_compra/tests_model_compra.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
+
+from cliente.models import Cliente
 from compra.models import Compra
+from produto.models import Produto
+
 # Create your tests here.
 
 
@@ -8,28 +12,47 @@ class TestModelCompras(TestCase):
 
     # Função responsável por inicializar os testes (Inicializado antes de rodar os outros testes)
     def setUp(self):
+        self.cliente=Cliente.objects.create(
+            cpf='100.000.000-90',
+            nome='Rodrigo de Freitas',
+            cep='37540-000',
+            email='rodrigao_frei28@hotmail.com',
+            senha='rodrigao_o_mais_brabo',
+            telefone='35 9.9070-8090'
+        )
+
+        self.produto=Produto.objects.create(
+            idProduto=1,
+            nome='Parafusadeira',
+            marca='Bosch',
+            preco=175.99,
+            quantidade=100
+        )
 
         self.compra=Compra.objects.create(
             idCompra=1,
+            preco=self.produto.preco,
+            tipoPagamento='boleto',
+            codigoRastreamento='BR099089AM',
+            quantidadeCoprada=25,
+            cliente_cpf=self.cliente,
+            produto_id=self.produto
+
         )
 
     ## TESTES UNITÁRIOS ##
+    def test_str_compra_tipo_pagamento(self):
+        self.assertEqual('boleto',str(self.compra.tipoPagamento))
 
+    def test_total_compra(self):
+        self.assertEqual(4179.7625,round(self.compra.calcula_total(),4))
 
     ## Testes de integração ##
-    ''''
-    def test_str_cliente_retornar_nome(self):
-        self.assertEqual('Rodrigo de Freitas',str(self.cliente.nome))
 
 
-    def test_str_cliente_retornar_email(self):
-        self.assertEqual('rodrigao_frei28@hotmail.com',str(self.cliente.email))
+    def test_istancia_produto_id(self):
+        self.assertIsInstance(self.compra.produto_id,Produto)
 
-
-    def test_str_cliente_retornar_telefone(self):
-        self.assertEqual('35 9.9070-8090',str(self.cliente.telefone))
-
-    def test_str_cliente_retornar_cep(self):
-        self.assertEqual('37540-000',str(self.cliente.cep))
-    '''
+    def test_istancia_cliente_cpf(self):
+        self.assertIsInstance(self.compra.cliente_cpf, Cliente)
 
