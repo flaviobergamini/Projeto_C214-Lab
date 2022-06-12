bedfa1237616aeacf6b84fd40e755c146746bd52
G-Ilian <gbiel.boza@gmail.com>
2022-06-11 23:37:48 -0300

    Diretório onde são realizados os testes do app de produtos

---
+++ vendas/produto/tests_produto/tests_model_produto.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from produto.models import Produto
+
+# Create your tests here.
+
+
+
+class TestModelCompras(TestCase):
+
+    # Função responsável por inicializar os testes (Inicializado antes de rodar os outros testes)
+    def setUp(self):
+        self.produto = Produto.objects.create(
+            idProduto=1,
+            nome='Parafusadeira',
+            marca='Bosch',
+            preco=175.99,
+            quantidade=100
+        )
+
+    ## TESTES UNITÁRIOS ##
+    def test_qtd_produto(self):
+        self.assertEqual(100, self.produto.quantidade)
+
+    def test_marca_produto(self):
+        self.assertEqual('Bosch', str(self.produto.marca))
+
+    def test_valor_total_estoque(self):
+        self.assertEqual(14959.15, self.produto.calcula_total())
+
+    ## TESTES DE INTEGRAÇÃO ##
+
+
