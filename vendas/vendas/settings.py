2d968bf384ab96a3d8564ec369310d6d3323c82e
G-Ilian <gbiel.boza@gmail.com>
2022-06-22 13:48:29 -0300

    Configurando novamente o heroku para permitir a passagem dos testes no CI

--- vendas/vendas/settings.py
+++ vendas/vendas/settings.py
@@ -133,4 +133,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+django_heroku.settings(locals(),test_runner=False)
